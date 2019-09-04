6251aa2f74325e4fabad5228f2c26c5d6f8519f3
henrypp <support@henrypp.org>
2019-09-04 16:28:01 +0600

    upd example

--- bin/example.bat
+++ bin/example.bat
@@ -1,3 +1,4 @@
 @echo off
-%~dp0hostsmgr -ip 0.0.0.0 -os win -path .\hosts
+"%~dp0hostsmgr.exe" -ip 0.0.0.0 -os win -path "%~dp0hosts"
+rem "%~dp0hostsmgr.exe" -ip 0.0.0.0 -os linux -path "%~dp0hosts"
 pause
