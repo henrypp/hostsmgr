762d5b2d90f8ae22416eb3831b979422fc652292
henrypp <support@henrypp.org>
2019-12-03 08:52:25 +0600

    upd config

--- src/main.hpp
+++ src/main.hpp
@@ -6,10 +6,31 @@
 
 #include <windows.h>
 #include <commctrl.h>
+
+#include "routine.hpp"
 #include "resource.hpp"
 #include "app.hpp"
 
 // libs
-#pragma comment(lib, "iphlpapi.lib")
+//#pragma comment(lib, "iphlpapi.lib")
+
+struct STATIC_DATA
+{
+	bool is_nobackup = false;
+	bool is_noresolver = false;
+
+	WCHAR hosts_file[MAX_PATH] = {0};
+	WCHAR eol[3] = {0};
+
+	LPWSTR sources_file = {0};
+	LPWSTR whitelist_file = {0};
+	LPWSTR userlist_file = {0};
+	LPWSTR cache_dir = {0};
+	LPWSTR hosts_destination = nullptr;
+
+
+	HANDLE houtput = nullptr;
+	WORD attributes = 0;
+};
 
 #endif // __MAIN_H__
