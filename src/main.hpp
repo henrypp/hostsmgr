3e5b3991ed9280e390b9b5ae68872f6a1dc0bab0
henrypp <support@henrypp.org>
2020-01-03 11:49:06 +0600

    upd config struct

--- src/main.hpp
+++ src/main.hpp
@@ -1,8 +1,7 @@
 // hostsmgr
-// Copyright (c) 2016-2019 Henry++
+// Copyright (c) 2016-2020 Henry++
 
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#pragma once
 
 #include <windows.h>
 #include <commctrl.h>
@@ -16,21 +15,21 @@
 
 struct STATIC_DATA
 {
-	bool is_nobackup = false;
-	bool is_noresolver = false;
-
 	WCHAR hosts_file[MAX_PATH] = {0};
 	WCHAR eol[3] = {0};
 
-	LPWSTR sources_file = {0};
-	LPWSTR whitelist_file = {0};
-	LPWSTR userlist_file = {0};
-	LPWSTR cache_dir = {0};
+	LPWSTR sources_file = nullptr;
+	LPWSTR whitelist_file = nullptr;
+	LPWSTR userlist_file = nullptr;
+	LPWSTR cache_dir = nullptr;
 	LPWSTR hosts_destination = nullptr;
-
+	LPWSTR hosts_file_temp = nullptr;
 
 	HANDLE houtput = nullptr;
-	WORD attributes = 0;
-};
 
-#endif // __MAIN_H__
+	WORD attributes = 0;
+
+	bool is_nobackup = false;
+	bool is_noresolver = false;
+	bool is_nocache = false;
+};
