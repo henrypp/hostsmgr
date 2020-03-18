673fde7e7266c9cdb6407ca04e1db8f46646e423
henrypp <support@henrypp.org>
2020-03-18 13:02:28 +0600

    fix running example

--- bin/example.bat
+++ bin/example.bat
@@ -1,4 +1,4 @@
 @echo off
-"%~dp0hostsmgr.exe" -ip 0.0.0.0 -os win -path "%~dp0hosts"
-rem "%~dp0hostsmgr.exe" -ip 0.0.0.0 -os linux -path "%~dp0hosts"
+hostsmgr.exe -ip 0.0.0.0 -os win -path ".\hosts_windows"
+rem hostsmgr.exe -ip 0.0.0.0 -os linux -path ".\hosts_linux"
 pause
