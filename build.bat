6e552d3ab44bb35384e07642291cef4c680de4cf
henrypp <support@henrypp.org>
2021-11-03 17:08:24 +0600

    ++version

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd ..\builder
-call build hostsmgr 2.1.1
+call build hostsmgr 2.1.2
 
 pause
--- src/app.h
+++ src/app.h
@@ -5,7 +5,7 @@
 
 #define APP_NAME L"hostsmgr"
 #define APP_NAME_SHORT L"hostsmgr"
-#define APP_VERSION L"2.1.1"
-#define APP_VERSION_RES 2,1,1,0
+#define APP_VERSION L"2.1.2"
+#define APP_VERSION_RES 2,1,2,0
 #define APP_AUTHOR L"Henry++"
 #define APP_COPYRIGHT L"(c) 2016-2021 " APP_AUTHOR L". All Rights Reversed."
