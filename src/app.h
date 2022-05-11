1101b3edd0974aa9fb9a30f123b549422b9347ed
henrypp <support@henrypp.org>
2022-05-11 07:18:30 +0600

    upd year

--- src/app.h
+++ src/app.h
@@ -1,5 +1,5 @@
 // hostsmgr
-// Copyright (c) 2016-2021 Henry++
+// Copyright (c) 2016-2022 Henry++
 
 #pragma once
 
@@ -8,4 +8,4 @@
 #define APP_VERSION L"2.2"
 #define APP_VERSION_RES 2,2,0,0
 #define APP_AUTHOR L"Henry++"
-#define APP_COPYRIGHT L"(c) 2016-2021 " APP_AUTHOR L". All Rights Reversed."
+#define APP_COPYRIGHT L"(c) 2016-2022 " APP_AUTHOR L". All Rights Reversed."
